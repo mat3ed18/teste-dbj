1832ee6ad1ac0199de8d2da3a3a5e3021e2e92b7
mat3ed18 <mathiaspedro9@gmail.com>
2021-10-02 00:23:46 +0000

    trabalhando a parte do back e do front-end

--- js/main.js
+++ js/main.js
@@ -1,25 +1,130 @@
 "use strict";
 var tarefas;
 (function (tarefas) {
-    var usuario = new tarefas.Usuario(1, "Renato dos Anjos");
-    document.body.innerHTML += "nome do usuário: " + usuario.nome;
-    var tarefa1 = new tarefas.Tarefa(1, "Teste 1", tarefas.TarefaEstado.PENDENTE);
-    var tarefa2 = new tarefas.Tarefa(2, "Teste 2", tarefas.TarefaEstado.EXECUCAO);
-    var tarefa3 = new tarefas.Tarefa(3, "Teste 3", tarefas.TarefaEstado.FINALIZADA);
-    usuario.adicionarTarefa(tarefa1);
-    usuario.adicionarTarefa(tarefa2);
-    usuario.adicionarTarefa(tarefa3);
-    usuario.estadoTarefa(1, tarefas.TarefaEstado.FINALIZADA);
-    usuario.removerTarefa(2);
-    usuario.tarefas.forEach(function (tarefa) {
-        document.body.innerHTML += "<br>tarefa: " + tarefa.descricao + " - " + tarefa.estado;
-    });
+    // const usuario = new Usuario(1, "Renato dos Anjos");
+    // document.body.innerHTML += "nome do usuário: " + usuario.nome;
+    // const tarefa1 = new Tarefa(1, "Teste 1", TarefaEstado.PENDENTE);
+    // const tarefa2 = new Tarefa(2, "Teste 2", TarefaEstado.EXECUCAO);
+    // const tarefa3 = new Tarefa(3, "Teste 3", TarefaEstado.FINALIZADA);
+    // usuario.adicionarTarefa(tarefa1);
+    // usuario.adicionarTarefa(tarefa2);
+    // usuario.adicionarTarefa(tarefa3);
+    // usuario.estadoTarefa(1, TarefaEstado.FINALIZADA);
+    // usuario.removerTarefa(2);
+    // usuario.tarefas.forEach(tarefa => {
+    //     document.body.innerHTML += "<br>tarefa: " + tarefa.descricao + " - " + tarefa.estado;
+    // });
     $(document).on("click", ".addTask", function () {
+        var usuarios = JSON.parse(localStorage.getItem("usuarios"));
+        $("#usuarioTarefa").html("<option value=\"\" selected>Selecione um autor</option>");
+        for (var i = 0; i < usuarios.length; i++) {
+            $("#usuarioTarefa").append("<option value=\"" + usuarios[i]._id + "\">" + usuarios[i]._nome + "</option>");
+        }
         $(".adicionar_tarefa").modal("show");
     });
     $(document).on("click", ".addUser", function () {
         $(".adicionar_usuario").modal("show");
     });
+    function main_tasks() {
+        var usuarios = JSON.parse(localStorage.getItem("usuarios"));
+        var html_task = "";
+        $(".main_tasks").html("");
+        if (localStorage.getItem("usuarios") != null) {
+            for (var i = 0; i < usuarios.length; i++) {
+                for (var j = 0; j < usuarios[i]._tarefas.length; j++) {
+                    if (usuarios[i]._tarefas[j] != null) {
+                        switch (usuarios[i]._tarefas[j]._estado) {
+                            case "Pendente":
+                            default:
+                                html_task += "<div class=\"card col-4 text-dark bg-light mb-3\" style=\"max-width: 18rem;\">";
+                                break;
+                            case "Em execução":
+                                html_task += "<div class=\"card col-4 text-dark bg-success mb-3\" style=\"max-width: 18rem;\">";
+                                break;
+                            case "Finalizada":
+                                html_task += "<div class=\"card col-4 text-dark bg-danger mb-3\" style=\"max-width: 18rem;\">";
+                                break;
+                        }
+                        html_task += "<div class=\"card-header font-weight-bold text-white\">Tarefa #11</div>";
+                        html_task += "<div class=\"card-body\">";
+                        html_task += "<p class=\"card-text text-white\">" + usuarios[i]._tarefas[j]._descricao + "</p>";
+                        html_task += "<a id-task=\"" + usuarios[i]._tarefas[j]._id + "\" class=\"btn btn-secondary btnUpdatePending\"><i class=\"bi bi-clock\"></i></a>";
+                        html_task += "<a id-task=\"" + usuarios[i]._tarefas[j]._id + "\" class=\"btn btn-primary btnUpdateCurrent\"><i class=\"bi bi-check2\"></i></a>";
+                        html_task += "<a id-task=\"" + usuarios[i]._tarefas[j]._id + "\" class=\"btn btn-outline-dark btnUpdateDone\"><i class=\"bi bi-check2-all\"></i></a>";
+                        html_task += "<a id-task=\"" + usuarios[i]._tarefas[j]._id + "\" class=\"btn btn-danger btnDeleteTask\"><i class=\"bi bi-trash\"></i></a>";
+                        html_task += "</div>";
+                        html_task += "</div>";
+                    }
+                }
+            }
+            $(".main_tasks").html(html_task);
+        }
+        else {
+            $(".main_tasks").html("Você não tem nenhuma tarefa");
+        }
+    }
+    $(document).ready(function () {
+        main_tasks();
+    });
+    function events_tasks() {
+        $(document).on("click", ".card .btnUpdatePending", function () {
+            var usuarios = JSON.parse(localStorage.getItem("usuarios"));
+            for (var i = 0; i < usuarios.length; i++) {
+                for (var j = 0; j < usuarios[i]._tarefas.length; j++) {
+                    if (usuarios[i]._tarefas[j] != null) {
+                        if (usuarios[i]._tarefas[j]._id == parseInt($(this).attr("id-task"))) {
+                            usuarios[i]._tarefas[j]._estado = tarefas.TarefaEstado.PENDENTE;
+                        }
+                    }
+                }
+            }
+            localStorage.setItem("usuarios", JSON.stringify(usuarios));
+            main_tasks();
+        });
+        $(document).on("click", ".card .btnUpdateCurrent", function () {
+            var usuarios = JSON.parse(localStorage.getItem("usuarios"));
+            for (var i = 0; i < usuarios.length; i++) {
+                for (var j = 0; j < usuarios[i]._tarefas.length; j++) {
+                    if (usuarios[i]._tarefas[j] != null) {
+                        if (usuarios[i]._tarefas[j]._id == parseInt($(this).attr("id-task"))) {
+                            usuarios[i]._tarefas[j]._estado = tarefas.TarefaEstado.EXECUCAO;
+                        }
+                    }
+                }
+            }
+            localStorage.setItem("usuarios", JSON.stringify(usuarios));
+            main_tasks();
+        });
+        $(document).on("click", ".card .btnUpdateDone", function () {
+            var usuarios = JSON.parse(localStorage.getItem("usuarios"));
+            for (var i = 0; i < usuarios.length; i++) {
+                for (var j = 0; j < usuarios[i]._tarefas.length; j++) {
+                    if (usuarios[i]._tarefas[j] != null) {
+                        if (usuarios[i]._tarefas[j]._id == parseInt($(this).attr("id-task"))) {
+                            usuarios[i]._tarefas[j]._estado = tarefas.TarefaEstado.FINALIZADA;
+                        }
+                    }
+                }
+            }
+            localStorage.setItem("usuarios", JSON.stringify(usuarios));
+            main_tasks();
+        });
+        $(document).on("click", ".card .btnDeleteTask", function () {
+            var usuarios = JSON.parse(localStorage.getItem("usuarios"));
+            for (var i = 0; i < usuarios.length; i++) {
+                for (var j = 0; j < usuarios[i]._tarefas.length; j++) {
+                    if (usuarios[i]._tarefas[j] != null) {
+                        if (usuarios[i]._tarefas[j]._id == parseInt($(this).attr("id-task"))) {
+                            delete usuarios[i]._tarefas[j];
+                        }
+                    }
+                }
+            }
+            localStorage.setItem("usuarios", JSON.stringify(usuarios));
+            main_tasks();
+        });
+    }
+    events_tasks();
     $(document).on("click", ".adicionar_usuario .btn-outline-success", function () {
         if (localStorage.getItem("usuarios") != null) {
             var usuarios = JSON.parse(localStorage.getItem("usuarios"));
@@ -39,8 +144,37 @@ var tarefas;
         $(".alert").modal("show");
     });
     $(document).on("click", ".adicionar_tarefa .btn-outline-success", function () {
-        $(".adicionar_tarefa").modal("hide");
-        $("#alertMsg").html("A tarefa foi cadastrada com sucesso");
-        $(".alert").modal("show");
+        if (localStorage.getItem("usuarios") != null) {
+            var usuarios = JSON.parse(localStorage.getItem("usuarios"));
+            for (var i = 0; i < usuarios.length; i++) {
+                if (usuarios[i]._id == $("#usuarioTarefa").val()) {
+                    var random_id = Math.floor(Math.random() * 10000) + 1;
+                    switch ($("#estadoTarefa").val()) {
+                        case "PENDENTE":
+                            usuarios[i]._tarefas.push(new tarefas.Tarefa(random_id, $("#descricao").val().toString(), tarefas.TarefaEstado.PENDENTE));
+                            break;
+                        case "EM ANDAMENTO":
+                            usuarios[i]._tarefas.push(new tarefas.Tarefa(random_id, $("#descricao").val().toString(), tarefas.TarefaEstado.EXECUCAO));
+                            break;
+                        case "FINALIZADA":
+                            usuarios[i]._tarefas.push(new tarefas.Tarefa(random_id, $("#descricao").val().toString(), tarefas.TarefaEstado.FINALIZADA));
+                            break;
+                    }
+                }
+            }
+            localStorage.setItem("usuarios", JSON.stringify(usuarios));
+            $(".adicionar_tarefa").modal("hide");
+            $("#alertMsg").html("A tarefa foi cadastrada com sucesso");
+            $("#nameUser").val(null);
+            $(".alert").modal("show");
+            main_tasks();
+        }
+        else {
+            $(".adicionar_tarefa").modal("hide");
+            $("#alertMsg").html("<small>Adicione um usuário ou faça seu cadastro para adicionar tarefas</small>");
+            $("#nameUser").val(null);
+            $(".alert").modal("show");
+        }
+        events_tasks();
     });
 })(tarefas || (tarefas = {}));
--- ts/main.ts
+++ ts/main.ts
@@ -1,26 +1,33 @@
 namespace tarefas {
     
-    const usuario = new Usuario(1, "Renato dos Anjos");
+    // const usuario = new Usuario(1, "Renato dos Anjos");
 
-    document.body.innerHTML += "nome do usuário: " + usuario.nome;
+    // document.body.innerHTML += "nome do usuário: " + usuario.nome;
 
-    const tarefa1 = new Tarefa(1, "Teste 1", TarefaEstado.PENDENTE);
-    const tarefa2 = new Tarefa(2, "Teste 2", TarefaEstado.EXECUCAO);
-    const tarefa3 = new Tarefa(3, "Teste 3", TarefaEstado.FINALIZADA);
+    // const tarefa1 = new Tarefa(1, "Teste 1", TarefaEstado.PENDENTE);
+    // const tarefa2 = new Tarefa(2, "Teste 2", TarefaEstado.EXECUCAO);
+    // const tarefa3 = new Tarefa(3, "Teste 3", TarefaEstado.FINALIZADA);
 
-    usuario.adicionarTarefa(tarefa1);
-    usuario.adicionarTarefa(tarefa2);
-    usuario.adicionarTarefa(tarefa3);
+    // usuario.adicionarTarefa(tarefa1);
+    // usuario.adicionarTarefa(tarefa2);
+    // usuario.adicionarTarefa(tarefa3);
 
-    usuario.estadoTarefa(1, TarefaEstado.FINALIZADA);
+    // usuario.estadoTarefa(1, TarefaEstado.FINALIZADA);
 
-    usuario.removerTarefa(2);
+    // usuario.removerTarefa(2);
 
-    usuario.tarefas.forEach(tarefa => {
-        document.body.innerHTML += "<br>tarefa: " + tarefa.descricao + " - " + tarefa.estado;
-    });
+    // usuario.tarefas.forEach(tarefa => {
+    //     document.body.innerHTML += "<br>tarefa: " + tarefa.descricao + " - " + tarefa.estado;
+    // });
 
     $(document).on("click", ".addTask", function(){
+        const usuarios = JSON.parse(localStorage.getItem("usuarios"));
+
+        $("#usuarioTarefa").html("<option value=\"\" selected>Selecione um autor</option>");
+
+        for (let i = 0; i < usuarios.length; i++) {
+            $("#usuarioTarefa").append("<option value=\"" + usuarios[i]._id + "\">" + usuarios[i]._nome + "</option>");
+        }
         $(".adicionar_tarefa").modal("show");
     });
     
@@ -28,23 +35,125 @@ namespace tarefas {
         $(".adicionar_usuario").modal("show");
     });
 
+    function main_tasks() {
+        const usuarios = JSON.parse(localStorage.getItem("usuarios"));let html_task = "";
+
+        $(".main_tasks").html("");
+
+        if (localStorage.getItem("usuarios") != null) {
+            for (let i = 0; i < usuarios.length; i++) {
+                for (let j = 0; j < usuarios[i]._tarefas.length; j++) {
+                    if (usuarios[i]._tarefas[j] != null) {
+                        switch (usuarios[i]._tarefas[j]._estado) {
+                            case "Pendente":
+                            default:
+                                html_task += "<div class=\"card col-4 text-dark bg-light mb-3\" style=\"max-width: 18rem;\">";
+                            break;
+                            case "Em execução":
+                                html_task += "<div class=\"card col-4 text-dark bg-success mb-3\" style=\"max-width: 18rem;\">";
+                            break;
+                            case "Finalizada":
+                                html_task += "<div class=\"card col-4 text-dark bg-danger mb-3\" style=\"max-width: 18rem;\">";
+                            break;
+                        }
+
+                            html_task += "<div class=\"card-header font-weight-bold text-white\">Tarefa #11</div>";
+                            html_task += "<div class=\"card-body\">";
+                                html_task += "<p class=\"card-text text-white\">" + usuarios[i]._tarefas[j]._descricao + "</p>";
+                                html_task += "<a id-task=\"" + usuarios[i]._tarefas[j]._id + "\" class=\"btn btn-secondary btnUpdatePending\"><i class=\"bi bi-clock\"></i></a>";
+                                html_task += "<a id-task=\"" + usuarios[i]._tarefas[j]._id + "\" class=\"btn btn-primary btnUpdateCurrent\"><i class=\"bi bi-check2\"></i></a>";
+                                html_task += "<a id-task=\"" + usuarios[i]._tarefas[j]._id + "\" class=\"btn btn-outline-dark btnUpdateDone\"><i class=\"bi bi-check2-all\"></i></a>";
+                                html_task += "<a id-task=\"" + usuarios[i]._tarefas[j]._id + "\" class=\"btn btn-danger btnDeleteTask\"><i class=\"bi bi-trash\"></i></a>";
+                            html_task += "</div>";
+                        html_task += "</div>";
+                    }
+                }
+            }
+            $(".main_tasks").html(html_task);
+        } else {
+            $(".main_tasks").html("Você não tem nenhuma tarefa");
+        }
+        
+    }
+
+    $(document).ready(function(){
+        main_tasks();
+    });
+
+    function events_tasks () {
+        $(document).on("click", ".card .btnUpdatePending", function(){
+            const usuarios = JSON.parse(localStorage.getItem("usuarios"));
+            for (let i = 0; i < usuarios.length; i++) {
+                for (let j = 0; j < usuarios[i]._tarefas.length; j++) {
+                    if (usuarios[i]._tarefas[j] != null) {
+                        if (usuarios[i]._tarefas[j]._id == parseInt($(this).attr("id-task"))) {
+                            usuarios[i]._tarefas[j]._estado = TarefaEstado.PENDENTE;
+                        }
+                    }
+                }
+            }
+            localStorage.setItem("usuarios", JSON.stringify(usuarios));
+            main_tasks();
+        });
+
+        $(document).on("click", ".card .btnUpdateCurrent", function(){
+            const usuarios = JSON.parse(localStorage.getItem("usuarios"));
+            for (let i = 0; i < usuarios.length; i++) {
+                for (let j = 0; j < usuarios[i]._tarefas.length; j++) {
+                    if (usuarios[i]._tarefas[j] != null) {
+                        if (usuarios[i]._tarefas[j]._id == parseInt($(this).attr("id-task"))) {
+                            usuarios[i]._tarefas[j]._estado = TarefaEstado.EXECUCAO;
+                        }
+                    }
+                }
+            }
+            localStorage.setItem("usuarios", JSON.stringify(usuarios));
+            main_tasks();
+        });
+
+        $(document).on("click", ".card .btnUpdateDone", function(){
+            const usuarios = JSON.parse(localStorage.getItem("usuarios"));
+            for (let i = 0; i < usuarios.length; i++) {
+                for (let j = 0; j < usuarios[i]._tarefas.length; j++) {
+                    if (usuarios[i]._tarefas[j] != null) {
+                        if (usuarios[i]._tarefas[j]._id == parseInt($(this).attr("id-task"))) {
+                            usuarios[i]._tarefas[j]._estado = TarefaEstado.FINALIZADA;
+                        }
+                    }
+                }
+            }
+            localStorage.setItem("usuarios", JSON.stringify(usuarios));
+            main_tasks();
+        });
+
+        $(document).on("click", ".card .btnDeleteTask", function(){
+            const usuarios = JSON.parse(localStorage.getItem("usuarios"));
+            for (let i = 0; i < usuarios.length; i++) {
+                for (let j = 0; j < usuarios[i]._tarefas.length; j++) {
+                    if (usuarios[i]._tarefas[j] != null) {
+                        if (usuarios[i]._tarefas[j]._id == parseInt($(this).attr("id-task"))) {
+                            delete usuarios[i]._tarefas[j];
+                        }
+                    }
+                }
+            }
+            localStorage.setItem("usuarios", JSON.stringify(usuarios));
+            main_tasks();
+        });
+    }
+
+    events_tasks();
+
     $(document).on("click", ".adicionar_usuario .btn-outline-success", function(){
         if (localStorage.getItem("usuarios") != null) {
-
             const usuarios = JSON.parse(localStorage.getItem("usuarios"));
-
             const random_id = Math.floor(Math.random() * 10000) + 1;
-
             usuarios.push(new Usuario(random_id, $("#nameUser").val().toString()));
-
             localStorage.setItem("usuarios", JSON.stringify(usuarios));
         } else {
             const usuarios = [];
-
             const random_id = Math.floor(Math.random() * 10000) + 1;
-
-            usuarios.push(new Usuario(random_id, $("#nameUser").val().toString()));
-
+            usuarios.push( new Usuario(random_id, $("#nameUser").val().toString()) );
             localStorage.setItem("usuarios", JSON.stringify(usuarios));
         }
 
@@ -55,12 +164,43 @@ namespace tarefas {
     });
     
     $(document).on("click", ".adicionar_tarefa .btn-outline-success", function(){
-        $(".adicionar_tarefa").modal("hide");
-        $("#alertMsg").html("A tarefa foi cadastrada com sucesso");
+        if (localStorage.getItem("usuarios") != null) {
+            const usuarios = JSON.parse(localStorage.getItem("usuarios"));
 
-        $(".alert").modal("show");
+            for (let i = 0; i < usuarios.length; i++) {
+                if (usuarios[i]._id == $("#usuarioTarefa").val()) {
+                    const random_id = Math.floor(Math.random() * 10000) + 1;
+
+                    switch ($("#estadoTarefa").val()) {
+                        case "PENDENTE":
+                            usuarios[i]._tarefas.push(new Tarefa(random_id, $("#descricao").val().toString(), TarefaEstado.PENDENTE));
+                        break;
+                        case "EM ANDAMENTO":
+                            usuarios[i]._tarefas.push(new Tarefa(random_id, $("#descricao").val().toString(), TarefaEstado.EXECUCAO));
+                        break;
+                        case "FINALIZADA":
+                            usuarios[i]._tarefas.push(new Tarefa(random_id, $("#descricao").val().toString(), TarefaEstado.FINALIZADA));
+                        break;
+                    }
+                }
+            }
+
+            localStorage.setItem("usuarios", JSON.stringify(usuarios));
+
+            $(".adicionar_tarefa").modal("hide");
+            $("#alertMsg").html("A tarefa foi cadastrada com sucesso");
+            $("#nameUser").val(null);
+            $(".alert").modal("show");
+            main_tasks();
+        } else {
+            $(".adicionar_tarefa").modal("hide");
+            $("#alertMsg").html("<small>Adicione um usuário ou faça seu cadastro para adicionar tarefas</small>");
+            $("#nameUser").val(null);
+            $(".alert").modal("show");
+        }
+        events_tasks();
     });
-    
+
     
 
 }
